845211a801ff9762d5c623a34856bdc301105ba0
Agrim Sharma <104200996+eccentrico@users.noreply.github.com>
2022-08-06 13:18:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+PROMISED_DOWN=100
+PROMISED_UP=10
+CHROME_DRIVER_PATH="C:\Dev\chromedriver"
+TWITTER_EMAIL="youaremyfire46@gmail.com"
+PASSWORD="johnwick4567"
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        go_button=self.driver.find_element_by_css_selector(".start-button a ")
+        go_button.click()
+        self.down=self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
+        self.up=self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
+    def get_twitter_tweet(self):
+        self.driver.get("https://twitter.com/login")
+        time.sleep(5)
+        email = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        password = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+
+        email.send_keys("youaremyfire46@gmail.com")
+        password.send_keys("johnwick4567")
+        time.sleep(3)
+
+        password.send_keys(Keys.ENTER)
+        tweet_compose = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        
+
+        tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+
+        
+
+    
+
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+bot.get_twitter_tweet()
+
+
+        
